4d328d2420f25ce48e2704b7c6a9a1633f0e7bf3
Ionut Birlad <birladionut@yahoo.it>
2020-03-18 13:07:02 +0100

    progresso

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 
+  $(".actions .fas.fa-search").click(function () {
+    var stringaCercata = $(".actions #search-text").val().toLowerCase();
+    // console.log(stringaCercata);
+    $.ajax({
+      url: "",
+      method: "GET",
+      success: function (data) {
+        
+      },
+      error: function (err) {
+        alert("Qualcosa è andato storto!");
+      }
+    });
+  });
 
 
 
@@ -11,5 +25,5 @@ $(document).ready(function () {
 
 
 
-  
+
 });
