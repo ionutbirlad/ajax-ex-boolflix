d49eade852d6d2a03b39369665e91636d6b450ba
Ionut Birlad <birladionut@yahoo.it>
2020-03-18 19:43:09 +0100

    aggiunti miglioramenti vari

--- js/main.js
+++ js/main.js
@@ -7,42 +7,54 @@ $(document).ready(function () {
 
 
   $(".actions .fas.fa-search").click(function () {
-    var stringaCercata = $(".actions #search-text").val().toLowerCase();
-    // console.log(stringaCercata);
-    var baseMovieUrl = "https://api.themoviedb.org/3";
-    var baseImageUrl = "https://image.tmdb.org/t/p";
-    $.ajax({
-      // url: "https://api.themoviedb.org/3/search/movie?api_key=33dde37d29b1d13faeb21d0fefcc0389&query=ritorno+al+futuro",
-      url: baseMovieUrl + "/search/movie",
-      data: {
-        api_key: "33dde37d29b1d13faeb21d0fefcc0389",
-        query: stringaCercata,
-        language: "it-IT"
-      },
-      method: "GET",
-      success: function (data) {
-        // console.log(data);
-        var films = data.results;
-        console.log(films);
-        for (var i = 0; i < films.length; i++) {
-          var film = {
-            image: baseImageUrl + "/w342/" + films[i].poster_path,
-            title: films[i].title,
-            original: films[i].original_title,
-            language: films[i].original_language,
-            rating: films[i].vote_average
-          };
-          var templateCompiled = template(film);
-          $(templateCompiled).insertAfter(".row .card:last-child");
-        }
-      },
-      error: function (err) {
-        alert("Qualcosa è andato storto!");
-      }
-    });
+    ricercaFilm();
+  });
+  $("#search-text").keypress(function (event) {
+    if (event.keyCode == 13) {
+      ricercaFilm();
+    }
   });
 
 
+
+  function ricercaFilm() {
+    var stringaCercata = $(".actions #search-text").val().toLowerCase();
+    $(".actions #search-text").val("");
+    // console.log(stringaCercata);
+    var baseMovieUrl = "https://api.themoviedb.org/3";
+    var baseImageUrl = "https://image.tmdb.org/t/p";
+    if (stringaCercata.trim().length > 0) {
+      $.ajax({
+        // url: "https://api.themoviedb.org/3/search/movie?api_key=33dde37d29b1d13faeb21d0fefcc0389&query=ritorno+al+futuro",
+        url: baseMovieUrl + "/search/movie",
+        data: {
+          api_key: "33dde37d29b1d13faeb21d0fefcc0389",
+          query: stringaCercata,
+          language: "it-IT"
+        },
+        method: "GET",
+        success: function (data) {
+          // console.log(data);
+          var films = data.results;
+          console.log(films);
+          for (var i = 0; i < films.length; i++) {
+            var film = {
+              image: baseImageUrl + "/w342/" + films[i].poster_path,
+              title: films[i].title,
+              original: films[i].original_title,
+              language: films[i].original_language,
+              rating: films[i].vote_average
+            };
+            var templateCompiled = template(film);
+            $(templateCompiled).insertAfter(".row .card:last-child");
+          }
+        },
+        error: function (err) {
+          alert("Qualcosa è andato storto!");
+        }
+      });
+    }
+  }
 
 
 
