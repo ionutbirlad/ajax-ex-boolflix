8123d5f7b6e3e4bfd564608a72f95e72e5ea23c7
Ionut Birlad <birladionut@yahoo.it>
2020-03-19 16:41:09 +0100

    aggiunta opzione di ricerca tra "film" e "serie tv"

--- js/main.js
+++ js/main.js
@@ -24,40 +24,78 @@ $(document).ready(function () {
     var baseMovieUrl = "https://api.themoviedb.org/3";
     var baseImageUrl = "https://image.tmdb.org/t/p";
     if (stringaCercata.trim().length > 0) {
-      $.ajax({
-        // url: "https://api.themoviedb.org/3/search/movie?api_key=33dde37d29b1d13faeb21d0fefcc0389&query=ritorno+al+futuro",
-        url: baseMovieUrl + "/search/movie",
-        data: {
-          api_key: "33dde37d29b1d13faeb21d0fefcc0389",
-          query: stringaCercata,
-          language: "it-IT"
-        },
-        method: "GET",
-        success: function (data) {
-          // console.log(data);
-          var films = data.results;
-          // console.log(films);
-          for (var i = 0; i < films.length; i++) {
-            var film = {
-              image: baseImageUrl + "/w342/" + films[i].poster_path,
-              title: films[i].title,
-              original: films[i].original_title,
-              language: films[i].original_language,
-              rating: Math.ceil(films[i].vote_average / 2)
-            };
-            var templateCompiled = template(film);
-            $(templateCompiled).insertAfter(".row.movies .card:last-child");
+      var moviesOrSeries = $(".type-of-search").val();
+      if (moviesOrSeries == "film") {
+        $.ajax({
+          // url: "https://api.themoviedb.org/3/search/movie?api_key=33dde37d29b1d13faeb21d0fefcc0389&query=ritorno+al+futuro",
+          url: baseMovieUrl + "/search/movie",
+          data: {
+            api_key: "33dde37d29b1d13faeb21d0fefcc0389",
+            query: stringaCercata,
+            language: "it-IT"
+          },
+          method: "GET",
+          success: function (data) {
+              // console.log(data);
+              var films = data.results;
+              // console.log(films);
+              for (var i = 0; i < films.length; i++) {
+                var film = {
+                  image: baseImageUrl + "/w342/" + films[i].poster_path,
+                  title: films[i].title,
+                  original: films[i].original_title,
+                  language: films[i].original_language,
+                  rating: Math.ceil(films[i].vote_average / 2)
+                };
+                var templateCompiled = template(film);
+                $(templateCompiled).insertAfter(".row.movies .card:last-child");
 
-            // GESTIONE STELLE RATING
-            var percentualeRating = (film.rating * 100) / 5;
-            console.log(percentualeRating);
-            $(".row.movies .card:last-child").find(".stars-inner").css( "width", percentualeRating + "%");
+                // GESTIONE STELLE RATING
+                var percentualeRating = (film.rating * 100) / 5;
+                // console.log(percentualeRating);
+                $(".row.movies .card:last-child").find(".stars-inner").css( "width", percentualeRating + "%");
+              }
+          },
+          error: function (err) {
+            alert("Qualcosa è andato storto!");
           }
-        },
-        error: function (err) {
-          alert("Qualcosa è andato storto!");
-        }
-      });
+        });
+      } else if (moviesOrSeries == "serietv") {
+        $.ajax({
+          // url: "https://api.themoviedb.org/3/search/movie?api_key=33dde37d29b1d13faeb21d0fefcc0389&query=ritorno+al+futuro",
+          url: baseMovieUrl + "/search/tv",
+          data: {
+            api_key: "33dde37d29b1d13faeb21d0fefcc0389",
+            query: stringaCercata,
+            language: "it-IT"
+          },
+          method: "GET",
+          success: function (data) {
+              // console.log(data);
+              var films = data.results;
+              // console.log(films);
+              for (var i = 0; i < films.length; i++) {
+                var film = {
+                  image: baseImageUrl + "/w342/" + films[i].poster_path,
+                  title: films[i].title,
+                  original: films[i].original_title,
+                  language: films[i].original_language,
+                  rating: Math.ceil(films[i].vote_average / 2)
+                };
+                var templateCompiled = template(film);
+                $(templateCompiled).insertAfter(".row.series .card:last-child");
+
+                // GESTIONE STELLE RATING
+                var percentualeRating = (film.rating * 100) / 5;
+                // console.log(percentualeRating);
+                $(".row.series .card:last-child").find(".stars-inner").css( "width", percentualeRating + "%");
+              }
+          },
+          error: function (err) {
+            alert("Qualcosa è andato storto!");
+          }
+        });
+      }
     }
   }
 
