e702a242abc876676717c5756075c629bc7102fb
Ionut Birlad <birladionut@yahoo.it>
2020-03-21 16:30:53 +0100

    aggiunte

--- js/main.js
+++ js/main.js
@@ -5,30 +5,50 @@ $(document).ready(function () {
   var template = Handlebars.compile(source);
   // DA RIPETERE SOLO UNA VOLTA PER TEMPLATE
 
+  var hiddenCard = $(".card.hidden").clone();
+
 
   $(".actions .fas.fa-search").click(function () {
+    if ($(".row.movies").hasClass("owl-loaded")) {
+      // $(".row.movies").removeClass("owl-loaded");
+      // $(".row.movies").removeClass("owl-theme");
+      $(".row.movies .owl-stage").empty();
+      $(".row.movies .owl-stage").append(hiddenCard);
+    }
+    // $(".row.movies .card.hidden").siblings().remove();
     ricercaFilm();
   });
   $("#search-text").keypress(function (event) {
     if (event.keyCode == 13) {
+      // if ($(".row.movies").hasClass("owl-loaded")) {
+      //   // $(".row.movies").removeClass("owl-loaded");
+      //   // $(".row.movies").removeClass("owl-theme");
+      //   // $(".row.movies").removeClass("owl-drag");
+      //   // $(".owl-item").parentsUntil(".row.movies").remove();
+      //   // $(".row.movies").find(".owl-nav").remove();
+      //   // $(".row.movies").find(".owl-dots").remove();
+      //   // $(".row.movies").append(hiddenCard);
+      //   $('.owl-carousel').trigger('refresh.owl.carousel');
+      // }
       ricercaFilm();
     }
   });
 
-
+// PARTI FISSE API
+  var baseMovieOrTvShowUrl = "https://api.themoviedb.org/3";
+  var baseImageUrl = "https://image.tmdb.org/t/p";
+// PARTI FISSE API
 
   function ricercaFilm() {
     var stringaCercata = $(".actions #search-text").val().toLowerCase();
     $(".actions #search-text").val("");
     // console.log(stringaCercata);
-    var baseMovieUrl = "https://api.themoviedb.org/3";
-    var baseImageUrl = "https://image.tmdb.org/t/p";
     if (stringaCercata.trim().length > 0) {
       var moviesOrSeries = $(".type-of-search").val();
       if (moviesOrSeries == "film") {
         $.ajax({
           // url: "https://api.themoviedb.org/3/search/movie?api_key=33dde37d29b1d13faeb21d0fefcc0389&query=ritorno+al+futuro",
-          url: baseMovieUrl + "/search/movie",
+          url: baseMovieOrTvShowUrl + "/search/movie",
           data: {
             api_key: "33dde37d29b1d13faeb21d0fefcc0389",
             query: stringaCercata,
@@ -47,8 +67,8 @@ $(document).ready(function () {
                   language: films[i].original_language,
                   rating: Math.ceil(films[i].vote_average / 2)
                 };
-                var templateCompiled = template(film);
-                $(templateCompiled).insertAfter(".row.movies .card:last-child");
+                  var templateCompiled = template(film);
+                  $(templateCompiled).insertAfter(".row.movies .card:last-child");
 
                 // GESTIONE STELLE RATING
                 var percentualeRating = (film.rating * 100) / 5;
@@ -77,7 +97,7 @@ $(document).ready(function () {
         });
       } else if (moviesOrSeries == "serietv") {
         $.ajax({
-          url: baseMovieUrl + "/search/tv",
+          url: baseMovieOrTvShowUrl + "/search/tv",
           data: {
             api_key: "33dde37d29b1d13faeb21d0fefcc0389",
             query: stringaCercata,
@@ -86,26 +106,26 @@ $(document).ready(function () {
           method: "GET",
           success: function (data) {
               // console.log(data);
-              var films = data.results;
+              var series = data.results;
               // console.log(films);
-              for (var i = 0; i < films.length; i++) {
-                var film = {
-                  image: baseImageUrl + "/w342/" + films[i].poster_path,
-                  title: films[i].name,
-                  original: films[i].original_name,
-                  language: films[i].original_language,
-                  rating: Math.ceil(films[i].vote_average / 2)
+              for (var i = 0; i < series.length; i++) {
+                var tvShow = {
+                  image: baseImageUrl + "/w342/" + series[i].poster_path,
+                  title: series[i].name,
+                  original: series[i].original_name,
+                  language: series[i].original_language,
+                  rating: Math.ceil(series[i].vote_average / 2)
                 };
-                var templateCompiled = template(film);
-                $(templateCompiled).insertAfter(".row.series .card:last-child");
+                  var templateCompiled = template(tvShow);
+                  $(templateCompiled).insertAfter(".row.series .card:last-child");
 
                 // GESTIONE STELLE RATING
-                var percentualeRating = (film.rating * 100) / 5;
+                var percentualeRating = (tvShow.rating * 100) / 5;
                 // console.log(percentualeRating);
                 $(".row.series .card:last-child").find(".stars-inner").css( "width", percentualeRating + "%");
 
                 // GESTIONE TITOLI
-                if (film.title == film.original) {
+                if (tvShow.title == tvShow.original) {
                   $(".row.series .card:last-child").find(".titolo").remove();
                 }
               }
